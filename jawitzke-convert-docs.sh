dcfe9f6e15a1afb9698ef9ec6bcceb107fa891ff
jawitzke <jawitzke@live.unc.edu>
2017-07-14 12:34:28 -0400

    basic set up

--- jawitzke-convert-docs.sh
+++ jawitzke-convert-docs.sh
@@ -1,13 +1,21 @@
-
-
+#!/bin/bash
+echo ""
 # Declare input argument as a variable
+INPUT=$1
+OUTPUT=$INPUT
 
 # Convert markdown to HTML
-
+pandoc -o $OUTPUT.html $INPUT
+echo "Created new file in HTML test.html"
+echo ""
 # Convert markdown to DOCX
-
+pandoc -o $OUTPUT.docx $INPUT
+echo "Created new file in DOCX test.html"
+echo ""
 # Convert markdown to ODT
-
+pandoc -o $OUTPUT.odt $INPUT
+echo "Created new file in ODT test.html"
+echo ""
 # Convert markdown to PDF
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
