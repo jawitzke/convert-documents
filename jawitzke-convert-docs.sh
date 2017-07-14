1d1242f35248de53d36cc66105ee0ff73925d1d8
jawitzke <jawitzke@live.unc.edu>
2017-07-14 12:51:28 -0400

    changed some stuff

--- jawitzke-convert-docs.sh
+++ jawitzke-convert-docs.sh
@@ -6,16 +6,18 @@ OUTPUT=$INPUT
 
 # Convert markdown to HTML
 pandoc -o $OUTPUT.html $INPUT
-echo "Created new file in HTML test.html"
+echo "Created new file in HTML $OUTPUT.html"
 echo ""
 # Convert markdown to DOCX
 pandoc -o $OUTPUT.docx $INPUT
-echo "Created new file in DOCX test.html"
+echo "Created new file in DOCX $OUTPUT.docx"
 echo ""
 # Convert markdown to ODT
 pandoc -o $OUTPUT.odt $INPUT
-echo "Created new file in ODT test.html"
+echo "Created new file in ODT $OUTPUT.odt"
 echo ""
 # Convert markdown to PDF
-
+pandoc $INPUT --latex-engine=xelatex -o $OUTPUT.pdf
+echo "Created new file in PDF $OUTPUT.pdf"
+echo ""
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
