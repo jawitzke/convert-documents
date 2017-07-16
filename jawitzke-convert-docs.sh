8eb03137bb8ffae9aa448790ad448085c7bd4030
jawitzke <jawitzke@live.unc.edu>
2017-07-16 14:55:21 -0400

    added rtf to convert

--- jawitzke-convert-docs.sh
+++ jawitzke-convert-docs.sh
@@ -2,22 +2,26 @@
 echo ""
 # Declare input argument as a variable
 INPUT=$1
-OUTPUT=$INPUT
+OUTPUT=$(echo -n $INPUT | head -c -3)
 
 # Convert markdown to HTML
 pandoc -o $OUTPUT.html $INPUT
-echo "Created new file in HTML $OUTPUT.html"
+echo "Created new file in HTML format called $OUTPUT.html"
 echo ""
 # Convert markdown to DOCX
 pandoc -o $OUTPUT.docx $INPUT
-echo "Created new file in DOCX $OUTPUT.docx"
+echo "Created new file in DOCX format called $OUTPUT.docx"
 echo ""
 # Convert markdown to ODT
 pandoc -o $OUTPUT.odt $INPUT
-echo "Created new file in ODT $OUTPUT.odt"
+echo "Created new file in ODT format called $OUTPUT.odt"
 echo ""
 # Convert markdown to PDF
-pandoc $INPUT --latex-engine=xelatex -o $OUTPUT.pdf
-echo "Created new file in PDF $OUTPUT.pdf"
+pandoc $INPUT --latex-engine=pdflatex -o $OUTPUT.pdf
+echo "Created new file in PDF fromat called $OUTPUT.pdf"
+echo ""
+#convert markdown to RTF
+pandoc -s $INPUT -o $OUTPUT.rtf
+echo "Created new file in RTF format called $OUTPUT.rtf"
 echo ""
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
